0335aa391569d30674083f0f57b9ed84ca85ae5f
Steev <steev@weirdresonance.com>
2017-03-31 08:04:30 +0100

    Added radiobutton listener.

--- app/src/main/java/com/weirdresoance/android/stargatequiz/QuizActivity.java
+++ app/src/main/java/com/weirdresoance/android/stargatequiz/QuizActivity.java
@@ -4,17 +4,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.view.View.OnClickListener;
+
 
 import static com.weirdresoance.android.stargatequiz.WelcomeActivity.userName;
 
 public class QuizActivity extends AppCompatActivity {
+
+    private RadioGroup radioGroup;
+    private RadioButton radioButton;
+    private Button btnDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
         displayName();
+
+        addListenerOnButton();
     }
 
     public void displayName() {
@@ -22,6 +33,30 @@ public class QuizActivity extends AppCompatActivity {
         name.setText(userName);
     }
 
+    public void addListenerOnButton() {
+
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroupQ1);
+        btnDisplay = (Button) findViewById(R.id.radioButtonQ1A);
+
+        btnDisplay.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                // get selected radio button from radioGroup
+                int selectedId = radioGroup.getCheckedRadioButtonId();
+
+                // find the radiobutton by returned id
+                radioButton = (RadioButton) findViewById(selectedId);
+
+                Toast.makeText(QuizActivity.this,
+                        radioButton.getText(), Toast.LENGTH_SHORT).show();
+
+            }
+
+        });
+
+    }
     public void hideView(View v) {
 
         // Oi!!! STEEV-----Get RadioButton checked state and save it to somewhere - An array maybe, or just variables!!!!!!!
