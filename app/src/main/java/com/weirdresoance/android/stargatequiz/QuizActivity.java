ed8cfbbae8f43445e82bc050b0b6698320d107c9
Steev <steev@weirdresonance.com>
2017-03-31 08:27:17 +0100

    Added radiobutton switch statement

--- app/src/main/java/com/weirdresoance/android/stargatequiz/QuizActivity.java
+++ app/src/main/java/com/weirdresoance/android/stargatequiz/QuizActivity.java
@@ -1,23 +1,23 @@
 package com.weirdresoance.android.stargatequiz;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.view.View.OnClickListener;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
 
 
 import static com.weirdresoance.android.stargatequiz.WelcomeActivity.userName;
 
 public class QuizActivity extends AppCompatActivity {
 
-    private RadioGroup radioGroup;
-    private RadioButton radioButton;
-    private Button btnDisplay;
+    RadioButton radioButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +25,31 @@ public class QuizActivity extends AppCompatActivity {
         setContentView(R.layout.activity_quiz);
         displayName();
 
-        addListenerOnButton();
+
+        RadioGroup rg = (RadioGroup) findViewById(R.id.radioGroupQ1);
+
+        rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
+        {
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch(checkedId){
+                    case R.id.radioButtonQ1A:
+                        // do operations specific to this selection
+                        radioButton = (RadioButton) findViewById(R.id.radioButtonQ1A);
+                        Toast.makeText(QuizActivity.this, radioButton.getText(), Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.radioButtonQ1B:
+                        // do operations specific to this selection
+                        break;
+                    case R.id.radioButtonQ1C:
+                        // do operations specific to this selection
+                        break;
+                    case R.id.radioButtonQ1D:
+                        // do operations specific to this selection
+                        break;
+                }
+            }
+        });
+
     }
 
     public void displayName() {
@@ -33,30 +57,6 @@ public class QuizActivity extends AppCompatActivity {
         name.setText(userName);
     }
 
-    public void addListenerOnButton() {
-
-        radioGroup = (RadioGroup) findViewById(R.id.radioGroupQ1);
-        btnDisplay = (Button) findViewById(R.id.radioButtonQ1A);
-
-        btnDisplay.setOnClickListener(new OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-
-                // get selected radio button from radioGroup
-                int selectedId = radioGroup.getCheckedRadioButtonId();
-
-                // find the radiobutton by returned id
-                radioButton = (RadioButton) findViewById(selectedId);
-
-                Toast.makeText(QuizActivity.this,
-                        radioButton.getText(), Toast.LENGTH_SHORT).show();
-
-            }
-
-        });
-
-    }
     public void hideView(View v) {
 
         // Oi!!! STEEV-----Get RadioButton checked state and save it to somewhere - An array maybe, or just variables!!!!!!!
@@ -73,3 +73,56 @@ public class QuizActivity extends AppCompatActivity {
         view.setVisibility(View.VISIBLE);
     }
 }
+
+
+
+/*
+import android.app.Activity;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.view.View.OnClickListener;
+        import android.widget.Button;
+        import android.widget.RadioButton;
+        import android.widget.RadioGroup;
+        import android.widget.Toast;
+
+public class MyAndroidAppActivity extends Activity {
+
+    private RadioGroup radioGroup;
+    private RadioButton radioButton;
+    private Button btnDisplay;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        addListenerOnButton();
+
+    }
+
+    public void addListenerOnButton() {
+
+        radioGroup = (RadioGroup) findViewById(R.id.radio);
+        btnDisplay = (Button) findViewById(R.id.btnDisplay);
+
+        btnDisplay.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                // get selected radio button from radioGroup
+                int selectedId = radioGroup.getCheckedRadioButtonId();
+
+                // find the radiobutton by returned id
+                radioButton = (RadioButton) findViewById(selectedId);
+
+                Toast.makeText(MyAndroidAppActivity.this,
+                        radioButton.getText(), Toast.LENGTH_SHORT).show();
+
+            }
+
+        });
+
+    }
+}*/
